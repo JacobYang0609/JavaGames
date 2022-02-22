d981b69f538fde8062b201a795ef38aa0d581bf4
Qiang (Jacob) Yang <79676187+JacobYang0609@users.noreply.github.com>
2022-02-21 21:35:29 -0500

    Add files via upload

---
+++ MultiplicationTable/NineNineMultiTable.java
@@ -0,0 +1,19 @@
+/*
+9-9 Multiplication Table
+*/
+
+class NineNineMultiTable 
+{
+	public static void main(String[] args) 
+	{
+		for (int i = 1; i <= 9; i++ )
+		{
+			for (int j = 1; j <= i ; j++ )
+			{
+				System.out.print(i + " * " + j + " = " + ( i * j) + "  ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
